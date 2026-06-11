3957b456be7b2aff304d2059b95106be056964b9
Eduardo Rojas <edurojas.developer@gmail.com>
2026-06-11 01:50:01 -0300

    refactor

--- js/components/card.js
+++ js/components/card.js
@@ -1,92 +1,19 @@
-const cardRender = ({...props}) => {
-  return `
-    <div class="card ${props.bg}">
+import{html,render}from"https://unpkg.com/lit-html?module";const themeComponent={light:"icon-light",dark:"icon",white:"#fff",black:"#000"};function cardRender({...i}){const{light:t,dark:e,white:s,black:l}=themeComponent,r=i.isLight?t:e,g=i.isLight?s:l;return html`
+    <div class="card ${i.bg}">
       <div class="card_description">
         <div>
-          <h3 class="title__h3 ${props.bgTitle}">${props?.firstTitle}</h3> <br>
-          <h3 class="title__h3 ${props.bgTitle}">${props?.secondTitle}</h3>
+          <h3 class="title__h3 ${i.bgTitle}">${i?.firstTitle}</h3> <br>
+          <h3 class="title__h3 ${i.bgTitle}">${i?.secondTitle}</h3>
         </div>
-        <a class="card__link" href="${props.toGoUrl}">
-          <img src="./assets/img/cards/${props.light ? 'icon-light' : 'icon'}.svg" alt="clicar">
-          <p style="color: ${props?.light ? '#fff' : '#000'}">Lean More</p>
+        <a class="card__link" href="${i.toGoUrl}">
+          <img src="./assets/img/cards/${r}.svg" alt="clicar">
+          <p style="color: ${g}">Lean More</p>
         </a>
       </div>
-
-      <img src="${props.imgUrl}" alt="Search" class="card__img">
+      <img src="${i.imgUrl}" alt="Search" class="card__img">
     </div>
-  `
-}
-
-const cardsDesc = [
-  {
-    bg: 'bg-white', 
-    bgTitle: 'bg-lemon', 
-    imgUrl: "./assets/img/cards/img-3.svg", 
-    toGoUrl: "#",
-    firstTitle: 'Search engine',
-    secondTitle: 'optimization',
-    light: false
-  },
-
-  {
-    bg: 'bg-lemon', 
-    bgTitle: 'bg-white', 
-    imgUrl: "./assets/img/cards/img-5.svg", 
-    toGoUrl: "#",
-    firstTitle: 'Pay-per-click',
-    secondTitle: 'optimization',
-    light: false
-  },
-
-  {
-    bg: 'bg-dark', 
-    bgTitle: 'bg-white', 
-    imgUrl: "./assets/img/cards/img.svg", 
-    toGoUrl: "#",
-    firstTitle: 'Social Media',
-    secondTitle: 'Marketing',
-    light: true
-  },
-
-  {
-    bg: null, 
-    bgTitle: 'bg-lemon', 
-    imgUrl: "./assets/img/cards/img-6.svg", 
-    toGoUrl: "#",
-    firstTitle: 'Email',
-    secondTitle: 'Marketing',
-    light: false
-  },
-
-  {
-    bg: 'bg-lemon', 
-    bgTitle: 'bg-lemon', 
-    imgUrl: "./assets/img/cards/img-4.svg", 
-    toGoUrl: "#",
-    firstTitle: 'Content',
-    secondTitle: 'Creation',
-    light: false
-  },
-
-  {
-    bg: 'bg-dark', 
-    bgTitle: 'bg-white', 
-    imgUrl: "./assets/img/cards/img-2.svg", 
-    toGoUrl: "#",
-    firstTitle: 'Analytics and',
-    secondTitle: 'Tracking',
-    light: true
-  }
-]
-
-// const classElement = document.querySelectorAll(".cardElement")
-// cardsDesc.map((item, index) => {
-//   classElement[index].innerHTML = cardRender({...item})
-// }).join('')
-
-const cardElement = document.getElementById("cards")
-cardElement.innerHTML = cardsDesc.map((item) => {
-  return cardRender({...item})
-}).join('')
-
-export default cardsDesc
+  `}const cardsDesc=[{bg:"bg-white",bgTitle:"bg-lemon",imgUrl:"./assets/img/cards/img-3.svg",toGoUrl:"#",firstTitle:"Search engine",secondTitle:"optimization",isLight:!1},{bg:"bg-lemon",bgTitle:"bg-white",imgUrl:"./assets/img/cards/img-5.svg",toGoUrl:"#",firstTitle:"Pay-per-click",secondTitle:"optimization",isLight:!1},{bg:"bg-dark",bgTitle:"bg-white",imgUrl:"./assets/img/cards/img.svg",toGoUrl:"#",firstTitle:"Social Media",secondTitle:"Marketing",isLight:!0},{bg:null,bgTitle:"bg-lemon",imgUrl:"./assets/img/cards/img-6.svg",toGoUrl:"#",firstTitle:"Email",secondTitle:"Marketing",isLight:!1},{bg:"bg-lemon",bgTitle:"bg-lemon",imgUrl:"./assets/img/cards/img-4.svg",toGoUrl:"#",firstTitle:"Content",secondTitle:"Creation",isLight:!1},{bg:"bg-dark",bgTitle:"bg-white",imgUrl:"./assets/img/cards/img-2.svg",toGoUrl:"#",firstTitle:"Analytics and",secondTitle:"Tracking",isLight:!0}],Cards=()=>html`
+  ${cardsDesc.map((i=>cardRender(i)))}
+`,cardElement=document.getElementById("cards");render(html`
+  ${cardsDesc.map((i=>cardRender(i)))}
+`,cardElement);export default cardsDesc;
--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
-import items from "./components/accordions.js";
-import brands from "./components/brands.js";
-import titles from "./components/tittle.js";
-import cardsDesc from "./components/card.js";
-import NavLinks from "./components/linksList.js";
-
-NavLinks()
+import items from"./components/accordions.js";import brands from"./components/brands.js";import titles from"./components/tittle.js";import cardsDesc from"./components/card.js";import NavLinks from"./components/linksList.js";NavLinks();
