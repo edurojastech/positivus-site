87aee6aa62766b863d4c4adcca53fe1cf1792018
Eduardo Rojas <edurojas.developer@gmail.com>
2026-06-11 02:07:34 -0300

    chore: ajustando imagnes

--- js/components/brands.js
+++ js/components/brands.js
@@ -1,18 +1,1 @@
-const brands = [
-  "./assets/img/logos/logo.svg",
-  "./assets/img/logos/logo-1.svg",
-  "./assets/img/logos/logo-2.svg",
-  "./assets/img/logos/logo-3.svg",
-  "./assets/img/logos/logo-4.svg",
-  "./assets/img/logos/logo-5.svg"
-]
-
-const marcas = document.querySelector(".brands")
-brands.forEach(element => {
-  const img = document.createElement("img")
-  img.src = `${element}`
-  img.alt = `${element}`
-  marcas.appendChild(img)
-});
-
-export default brands
+const brands=["./assets/img/logos/logo.svg","./assets/img/logos/logo-1.svg","./assets/img/logos/logo-2.svg","./assets/img/logos/logo-3.svg","./assets/img/logos/logo-4.svg","./assets/img/logos/logo-5.svg"],marcas=document.querySelector(".brands");brands.forEach((s=>{const o=document.createElement("img");o.src=`${s}`,o.alt=`${s}`,o.fetchPriority="high",marcas.appendChild(o)}));export default brands;
